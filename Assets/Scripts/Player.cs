81fba817d83e971115cf1aea76a67913b855e039
BrockPaperScissors <michaeljbrock91@gmail.com>
2026-05-07 01:22:22 -0700

    initial grappling hook functionality

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class Player : MonoBehaviour
 {
@@ -7,35 +8,86 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     public float moveSpeed = 10;
     public float jumpForce = 100f;
+    public float grappleSpeed = 20f;
     private bool isGrounded = false;
+    private bool canGrapple = true;
+    private bool canMove = true;
+    private Vector2 playerPos;
+    private GameObject castPoint;
+    private Vector2 clickPos = new Vector2(0, 0);
+    private Vector2 grappleDir;
+
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        castPoint = GameObject.Find("CastPoint");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+        if (canMove)
         {
-            transform.position += (Vector3.right * Time.deltaTime) * moveSpeed;
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += (Vector3.right * Time.deltaTime) * moveSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position += (Vector3.left * Time.deltaTime) * moveSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.Space) && isGrounded)
+            {
+
+
+                rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+
+                isGrounded = false;
+
+            }
+
+            if (Input.GetMouseButtonDown(1) && isGrounded)
+            {
+                // Debug.Log("right mouse button down clicked");
+                // clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                // playerPos = (Vector2)castPoint.transform.position;
+                // grappleDir = (clickPos - playerPos).normalized;
+
+                // int groundMask = LayerMask.GetMask("Ground");
+                // RaycastHit2D hit = Physics2D.Raycast(clickPos, playerPos, 100f, groundMask);
+
+                // if (hit.collider)
+                // {
+                //     Debug.Log("Hit  " + hit.collider.gameObject.name);
+
+                //     switch (hit.collider.gameObject.tag)
+                //     {
+                //         case ("Ground"):
+
+                //             Debug.Log("Grappling hit ground object");
+                //             canGrapple = false;
+                //             isGrounded = false;
+                //             canMove = false;
+
+                //             break;
+                //         default:
+                //             break;
+                //     }
+                // }
+
+                GrappleMove();
+            }
         }
 
-        if (Input.GetKey(KeyCode.A))
+        if (!canGrapple)
         {
-            transform.position += (Vector3.left * Time.deltaTime) * moveSpeed;
-        }
-
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
-        {
-
-
-            rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
-
-            isGrounded = false;
-
+            rb.MovePosition(rb.position + grappleDir * Time.deltaTime * grappleSpeed);
         }
     }
 
@@ -46,6 +98,41 @@ public class Player : MonoBehaviour
         {
             Debug.Log("Hit the ground");
             isGrounded = true;
+            canGrapple = true;
+            canMove = true;
+
         }
     }
+
+    void GrappleMove()
+    {
+        Debug.Log("right mouse button down clicked");
+        clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        playerPos = (Vector2)castPoint.transform.position;
+        grappleDir = (clickPos - playerPos).normalized;
+
+        int groundMask = LayerMask.GetMask("Ground");
+        RaycastHit2D hit = Physics2D.Raycast(clickPos, playerPos, 100f, groundMask);
+
+        if (hit.collider)
+        {
+            Debug.Log("Hit  " + hit.collider.gameObject.name);
+
+            switch (hit.collider.gameObject.tag)
+            {
+                case ("Ground"):
+
+                    Debug.Log("Grappling hit ground object");
+                    canGrapple = false;
+                    isGrounded = false;
+                    canMove = false;
+
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
 }
